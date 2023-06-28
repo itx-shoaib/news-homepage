6995f6ea6328e7b58b8c15eb25ac591dabcc243b
itx-shoaib <shoaibjamil43@gmail.com>
2023-06-29 00:02:46 +0500

    Making Navbar

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p className="text-gray-500 text-sm italic">
-        React and Tailwind CSS in action
-      </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <div>Body</div>
+      <div>Footer</div>
+    </>
   );
 }
 
---
+++ src/Components/assets/utils/NavbarData.js
@@ -0,0 +1,7 @@
+export  const NavbarData = [
+    {id:1,name:"Home"},
+    {id:2,name:"New"},
+    {id:3,name:"Popular"},
+    {id:4,name:"Trending"},
+    {id:5,name:"Categories"},
+]
